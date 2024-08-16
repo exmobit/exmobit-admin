8d239d161454520ec797a6a3bae612fe1b884134
Serhii <serhii.p@code-care.pro>
2024-08-16 21:04:25 +0300

    add new column

--- src/pages/Main/tabels/CurrenciesTable.tsx
+++ src/pages/Main/tabels/CurrenciesTable.tsx
@@ -1,77 +1,154 @@
 import { useEffect, useState } from "react";
 import useDatabase from "../../../database/useDatabase";
 
-type CurrencyEnum = 'name' | 'code' | 'inBTC';
+type CurrencyEnum = "name" | "code" | "inBTC"| "wallet";
 
 type Currency = {
-	name: string;
-	code: string;
-	inBTC: string;
-	id: string
-}
+  name: string;
+  code: string;
+  inBTC: string;
+  wallet: string;
+  id: string;
+};
 
 interface TableState extends Currency {
-	isUpdating: boolean;
-	newState: Currency | null;
+  isUpdating: boolean;
+  newState: Currency | null;
 }
 
 interface ICurrenciesTable {
-	currencies: Currency[],
-	addCurrencies: () => void,
-	removeCurrencies: (id: string) => void
-
+  currencies: Currency[];
+  addCurrencies: () => void;
+  removeCurrencies: (id: string) => void;
 }
 
 const CurrenciesTable = (props: ICurrenciesTable) => {
-	const [tableState, setTableState] = useState<TableState[]>([]);
+  const [tableState, setTableState] = useState<TableState[]>([]);
 
-	const db = useDatabase();
+  const db = useDatabase();
 
-	const update = (i: number) => {
-		const newTableState = [...tableState];
+  const update = (i: number) => {
+    const newTableState = [...tableState];
 
-		newTableState[i].isUpdating = !newTableState[i].isUpdating
+    newTableState[i].isUpdating = !newTableState[i].isUpdating;
 
-		if (!newTableState[i].isUpdating) {
-			db.updateData({ name: newTableState[i].name, code: newTableState[i].code, inBTC: newTableState[i].inBTC }, 'currencies', newTableState[i].id)
-		}
+    if (!newTableState[i].isUpdating) {
+      db.updateData(
+        {
+          name: newTableState[i].name,
+          code: newTableState[i].code,
+          inBTC: newTableState[i].inBTC,
+		  wallet: newTableState[i].wallet
+        },
+        "currencies",
+        newTableState[i].id
+      );
+    }
 
-		setTableState(newTableState)
-	}
+    setTableState(newTableState);
+  };
 
-	const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>, i: number, prop: CurrencyEnum) => {
-		const newTableState = [...tableState];
-		newTableState[i][prop] = event.target.value;
-		setTableState(newTableState)
-	}
+  const onChangeInput = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    i: number,
+    prop: CurrencyEnum
+  ) => {
+    const newTableState = [...tableState];
+    newTableState[i][prop] = event.target.value;
+    setTableState(newTableState);
+  };
 
+  useEffect(() => {
+    const initArr: TableState[] = [...props.currencies].map((el) => ({
+      ...el,
+      isUpdating: false,
+      newState: null,
+    }));
+    setTableState(initArr);
+  }, [props.currencies]);
 
-	useEffect(() => {
-		const initArr: TableState[] = [...props.currencies].map((el) => ({ ...el, isUpdating: false, newState: null }))
-		setTableState(initArr)
-	}, [props.currencies])
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>inBTC</th>
+          <th>code</th>
+          <th>wallet</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableState.map((curr, i) => (
+          <tr key={i}>
+            <td style={{ width: "20%" }}>
+              {tableState[i]?.isUpdating ? (
+                <input
+                  onChange={(e) => {
+                    onChangeInput(e, i, "name");
+                  }}
+                  value={tableState[i].name}
+                />
+              ) : (
+                curr.name
+              )}
+            </td>
+            <td style={{ width: "20%" }}>
+              {tableState[i]?.isUpdating ? (
+                <input
+                  onChange={(e) => {
+                    onChangeInput(e, i, "inBTC");
+                  }}
+                  value={tableState[i].inBTC}
+                />
+              ) : (
+                curr.inBTC
+              )}
+            </td>
+            <td style={{ width: "20%" }}>
+              {tableState[i]?.isUpdating ? (
+                <input
+                  onChange={(e) => {
+                    onChangeInput(e, i, "code");
+                  }}
+                  value={tableState[i].code}
+                />
+              ) : (
+                curr.code
+              )}
+            </td>
+			<td style={{ width: "20%" }}>
+              {tableState[i]?.isUpdating ? (
+                <input
+                  onChange={(e) => {
+                    onChangeInput(e, i, "wallet");
+                  }}
+                  value={tableState[i].wallet}
+                />
+              ) : (
+                curr.wallet
+              )}
+            </td>
+            <td>
+              <button
+                onClick={() => {
+                  update(i);
+                }}
+              >
+                {tableState[i]?.isUpdating ? "Save" : "Update"}
+              </button>{" "}
+              <button onClick={() => props.removeCurrencies(curr.id)}>
+                Remove
+              </button>
+            </td>
+          </tr>
+        ))}
+        <tr>
+          <button onClick={() => props.addCurrencies()}>Add</button>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
-	return <table>
-		<thead>
-			<tr>
-				<th>name</th>
-				<th>inBTC</th>
-				<th>code</th>
-				<th>action</th>
-			</tr>
-		</thead>
-		<tbody>
-			{tableState.map((curr, i) =>
-				<tr key={i}>
-					<td style={{ width: '20%' }}>{tableState[i]?.isUpdating ? <input onChange={(e) => { onChangeInput(e, i, 'name') }} value={tableState[i].name} /> : curr.name}</td>
-					<td style={{ width: '20%' }}>{tableState[i]?.isUpdating ? <input onChange={(e) => { onChangeInput(e, i, 'inBTC') }} value={tableState[i].inBTC} /> : curr.inBTC}</td>
-					<td style={{ width: '20%' }}>{tableState[i]?.isUpdating ? <input onChange={(e) => { onChangeInput(e, i, 'code') }} value={tableState[i].code} /> : curr.code}</td>
-					<td><button onClick={() => { update(i) }}>{tableState[i]?.isUpdating ? "Save" : "Update"}</button> <button onClick={() => props.removeCurrencies(curr.id)}>Remove</button></td>
-				</tr>
-			)}
-			<tr><button onClick={() => props.addCurrencies()}>Add</button></tr>
-		</tbody>
-	</table>
-}
-
-export default CurrenciesTable
+export default CurrenciesTable;
--- src/pages/Main/tabels/TransactionsTable.tsx
+++ src/pages/Main/tabels/TransactionsTable.tsx
@@ -1,53 +1,58 @@
 type Transaction = {
-	fromCurrency: {
-		name: string | undefined;
-		code: string | undefined;
-	};
-	toCurrency: {
-		name: string | undefined;
-		code: string | undefined;
-	};
-	created: Date;
-	status: string;
-	send: string | undefined;
-	receive: string | undefined;
-	wallet: string;
-	email: string;
-	id: string
-}
+  fromCurrency: {
+    name: string | undefined;
+    code: string | undefined;
+  };
+  toCurrency: {
+    name: string | undefined;
+    code: string | undefined;
+  };
+  created: Date;
+  status: string;
+  send: string | undefined;
+  receive: string | undefined;
+  wallet: string;
+  email: string;
+  id: string;
+  toWallet: string;
+};
 
 interface ITransactionsTable {
-	transactions: Transaction[],
-	remove: (id: string) => void
+  transactions: Transaction[];
+  remove: (id: string) => void;
 }
 
 const TransactionsTable = (props: ITransactionsTable) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>wallet</th>
+          <th>from</th>
+          <th>to</th>
+          <th>send</th>
+          <th>receive</th>
+          <th>toWallet</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.transactions.map((t, i) => (
+          <tr key={i}>
+            <td>{t.wallet}</td>
+            <td>{t.fromCurrency?.code}</td>
+            <td>{t.toCurrency?.code}</td>
+            <td>{t.send}</td>
+            <td>{t.receive}</td>
+            <td>{t.toWallet}</td>
+            <td>
+              <button onClick={() => props.remove(t.id)}>Remove</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-	return <table>
-		<thead>
-			<tr>
-				<th>username</th>
-				<th>wallet</th>
-				<th>from</th>
-				<th>to</th>
-				<th>send</th>
-				<th>receive</th>
-			</tr>
-		</thead>
-		<tbody>
-			{props.transactions.map((t, i) =>
-				<tr key={i}>
-					<td>{t.email}</td>
-					<td>{t.wallet}</td>
-					<td>{t.fromCurrency?.code}</td>
-					<td>{t.toCurrency?.code}</td>
-					<td>{t.send}</td>
-					<td>{t.receive}</td>
-					<td><button onClick={() => props.remove(t.id)}>Remove</button></td>
-				</tr>
-			)}
-		</tbody>
-	</table>
-}
-
-export default TransactionsTable
+export default TransactionsTable;
